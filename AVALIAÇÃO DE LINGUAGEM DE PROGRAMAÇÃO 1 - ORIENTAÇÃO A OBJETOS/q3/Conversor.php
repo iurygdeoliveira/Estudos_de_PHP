0fde165fd2ba7c33632b2f72ad7b418b40faaa56
João Lucas de Oliveira Lopes <jlucaso@hotmail.com>
2020-10-30 17:43:05 -0300

    Funcionando com regex

--- AVALIAÇÃO DE LINGUAGEM DE PROGRAMAÇÃO 1 - ORIENTAÇÃO A OBJETOS/q3/Conversor.php
+++ AVALIAÇÃO DE LINGUAGEM DE PROGRAMAÇÃO 1 - ORIENTAÇÃO A OBJETOS/q3/Conversor.php
@@ -1,11 +1,11 @@
 <?php
-
 class Conversor
 {
     function Conversor($from, $to, $string)
     {
         if (ctype_alpha($from) && strlen($from) >= 1 && strlen($from) <= 10 && $to >= 1 && $to <= 1000 && strlen($string) <= 50 && strlen($string) >= 1) {
-            echo str_ireplace($from, $to, $string);
+            echo preg_replace("/(?<=<|<\/)({$from})+(?=[^>]*>)/i", $to, $string);
+            echo "\n";
         }
     }
 }
--- AVALIAÇÃO DE LINGUAGEM DE PROGRAMAÇÃO 1 - ORIENTAÇÃO A OBJETOS/q3/index.php
+++ AVALIAÇÃO DE LINGUAGEM DE PROGRAMAÇÃO 1 - ORIENTAÇÃO A OBJETOS/q3/index.php
@@ -3,3 +3,7 @@ require_once "Conversor.php";
 $C1 = new Conversor("BODY", "10", "<><BODY garbage>body</BODY>");
 $C2 = new Conversor("aBc", "923", "<dont replacethis>abcabc<abcabcde>");
 $C3 = new Conversor("table", "1", "< ta>bLe< TaBlewidth=100></table></ta>");
+$C3 = new Conversor("replace", "323", "nothing inside");
+$C3 = new Conversor("HI", "667", "92< HI=/ >< z==//HIb >< cHIhi >");
+$C3 = new Conversor("a", "23", "<a B c a>");
+$C3 = new Conversor("b", "2", "<b b abc ab c> Mangojata");
