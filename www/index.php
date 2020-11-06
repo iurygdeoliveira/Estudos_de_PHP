e21bbbe1c9dad4f0285a999584c266505ebd5df8
Iury Gomes de Oliveira <iury.oliveira@ifto.edu.br>
2020-11-06 15:59:25 -0300

    Crud com PHP

---
+++ www/index.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+ob_start();
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+use model\Conectar;
+
+PHPClassName('PHP COM BANCO DE DADOS');
+
+
+PHPClassSession('CONNECT', __LINE__);
+
+$conexao = Conectar::getInstance();
+var_dump($conexao);
+
+PHPClassSession('INSERT', __LINE__);
+
+PHPClassSession('SELECT', __LINE__);
+
+PHPClassSession('UPDATE', __LINE__);
+
+PHPClassSession('DELETE', __LINE__);
+
+ob_end_flush();
